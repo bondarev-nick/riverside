f27ff646442ebed64f22ab06e92151ce3fbfa044
Nick Bondarev <>
2024-12-21 17:28:23 +0100

    added retrofit module and complete Hilt setup

--- app/src/main/java/com/example/riverside/MainActivity.kt
+++ app/src/main/java/com/example/riverside/MainActivity.kt
@@ -11,17 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.example.riverside.ui.theme.RiversideTheme
-import kotlinx.coroutines.launch
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        lifecycleScope.launch {
-        }
         setContent {
             RiversideTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "riverside"
 include(":app")
- 
+include(":retrofit")
